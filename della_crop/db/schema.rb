3a836734a643993bd700f106c1650e73f8450b39
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-13 09:30:58 -0400

    rake db

--- della_crop/db/schema.rb
+++ della_crop/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313132916) do
+ActiveRecord::Schema.define(version: 20170313133012) do
+
+  create_table "nurses", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string   "first_name"
