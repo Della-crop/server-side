4b3187d79b40c6b8786f11f61e85fa0188740e15
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-20 13:02:21 -0400

    routes

--- della_crop/config/routes.rb
+++ della_crop/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # add patient route
+  # update patient route
+  # alert patient fall down route
+  post '/alert' => 'patients#alert'
+  post '/call' => 'patients#call'
 end
