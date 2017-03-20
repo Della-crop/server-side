b824d385baaccb737af83eee99b6f1cecc66f874
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-20 10:18:13 -0400

    implementing add new patients

--- della_crop/app/controllers/patients_controller.rb
+++ della_crop/app/controllers/patients_controller.rb
@@ -1,6 +1,17 @@
 # blog posts controller
 class PatientsController < ApplicationController
-  def new; end
+  def new
+    @patient =  Patient.new(
+      params.require(:patient).permit(
+        :first_name,
+        :last_name,
+        :room_number,
+        :smartwatchSN)
+    )
+    @patient.save
+  end
+
+  def get; end
 
   def update; end
 end
