3d503b4cbe300088338c5180b328025b2fef6cfa
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-26 16:31:50 -0400

    added fetch, update and destory => routes

--- della_crop/config/routes.rb
+++ della_crop/config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # add patient route
-  # update patient route
   # alert patient fall down route
   post '/alert' => 'patients#alert'
   post '/call' => 'patients#call'
   get '/' => 'pages#home'
   get '/all' => 'patients#get_all_patient'
   post '/add' => 'patients#new'
-  # put '/update' => ''
-  # get '/patient' => ''
+  get '/patients/:first_name' => 'patients#fetch_patient_by_name'
+  get '/patients' => 'patients#fetch_all_patient'
+  get '/patients/:id' => 'patients#fetch_patient_by_id'
+  patch '/patients/:id' => 'patients#update'
+  delete '/patients/:id' => 'patients#destroy'
+  # TODO uncomment the alert route
+  # post '/alert/:id' => 'patients#alert'
 end
